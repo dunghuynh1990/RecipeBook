5f8262f084a276d13beff1aec962622587ff31f8
Huỳnh Trí Dũng <dunghuynh1990@gmail.com>
2018-06-08 16:16:01 +0800

    Implement SignIn feature

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../../services/auth';
 
 @IonicPage()
 @Component({
@@ -8,12 +9,34 @@ import { NgForm } from '@angular/forms';
   templateUrl: 'signin.html',
 })
 export class SigninPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private authService: AuthService,
+    private alertCtrl: AlertController,
+    private loadingCtrl: LoadingController
+  ) {}
 
   onSignup(form: NgForm){
-    console.log(form.value);
+    const loading = this.loadingCtrl.create({
+      content: 'Signing you in...'
+    });
+    loading.present();
+    this.authService.signin(form.value.email, form.value.password)
+    .then(data => {
+      loading.dismiss()
+    })
+    .catch(error => {
+      loading.dismiss();
+      const alert = this.alertCtrl.create({
+        title: 'Sign in failed!',
+        message: error.message,
+        buttons: ['OK']
+      });
+      alert.present();
+    });
+
   }
 
 }
